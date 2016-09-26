01e258f7dab065c1c05377ee41e302239651fb13
nica lazatin <tuhmaytow@gmail.com>
2016-09-26 12:28:19 -0700

    html edited

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-router.get('/', function(req, res, next) {
-
-
-  res.redirect('/index');
-});
-
 router.get('/front', function(req, res, next) {
-  //if user clicks create template button, redirect to index.html page
-
   res.redirect('/index');
 });
 
